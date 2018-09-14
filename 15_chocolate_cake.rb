a674a3f3b2d8a5ea694cfa6dd725f218e9eca734
Liam Giuliano-Kuhn <liam.gkuhn@gmail.com>
2018-09-14 10:26:42 +1000

    challenge 15 complete

--- 15_chocolate_cake.rb
+++ 15_chocolate_cake.rb
@@ -1,1 +1,21 @@
-# Write a program that prints the numbers from 1 to 100. For multiples of three print “Chocolate” instead of the number and for the multiples of five print “Cake”. For numbers which are multiples of both three and five print “ChocolateCake”.
+# Write a program that prints the numbers from 1 to 100.
+# For multiples of three print “Chocolate” instead of the number and for the multiples of five print “Cake”. 
+# For numbers which are multiples of both three and five print “ChocolateCake”.
+
+counter = 0
+while counter <= 100 do 
+    if 
+        ((counter % 3) == 0) && ((counter % 5) == 0)
+        puts "ChocolateCake"
+
+    elsif (counter % 5) == 0
+        puts "Cake"
+
+    elsif (counter % 3) == 0
+        puts "Chocolate"
+
+    else puts counter
+    end
+    counter +=1
+    
+end
